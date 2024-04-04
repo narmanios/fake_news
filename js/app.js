bb0e13710dad02d1bd403e14cb8126fbd1f03532
Neven Armanios <narmanios@gmail.com>
2024-04-04 15:53:18 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-import { bubblechart } from "./module_one.js";
-import { barchart } from "./module_two.js";
-import { heatmap } from "./module_three.js";
-import { scatterplot } from "./module_five.js";
+import { bubblechart } from "../module_one.js";
+import { barchart } from "../module_two.js";
+import { heatmap } from "../module_three.js";
+import { scatterplot } from "../module_five.js";
 
 bubblechart();
 barchart();
